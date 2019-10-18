f98abd03bd2b5ad0bf14bada4e5ce4159a06f03f
spreetzer <56721336+spreetzer@users.noreply.github.com>
2019-10-18 12:52:19 +0200

    Update shipping_methods_controller_decorator.rb

--- app/controllers/spree/admin/shipping_methods_controller_decorator.rb
+++ app/controllers/spree/admin/shipping_methods_controller_decorator.rb
@@ -1,16 +1,22 @@
-Spree::Admin::ShippingMethodsController.class_eval do
-  private
+module Spree
+  module Admin
+    module ShippingMethodsControllerDecorator
+      private
 
-  alias_method :old_set_shipping_category, :set_shipping_category
-  alias_method :old_set_zones, :set_zones
+      alias_method :old_set_shipping_category, :set_shipping_category
+      alias_method :old_set_zones, :set_zones
 
-  # We have to override those filters since they break proper PATCH behaviour.
+      # We have to override those filters since they break proper PATCH behaviour.
 
-  def set_shipping_category
-    old_set_shipping_category unless params['shipping_method'][:translations_attributes]
-  end
+      def set_shipping_category
+        old_set_shipping_category unless params['shipping_method'][:translations_attributes]
+      end
 
-  def set_zones
-    old_set_zones unless params['shipping_method'][:translations_attributes]
+      def set_zones
+        old_set_zones unless params['shipping_method'][:translations_attributes]
+      end
+    end
   end
 end
+
+(Spree::Admin::ShippingMethodsController.prepend Spree::Admin::ShippingMethodsControllerDecorator) if Spree.version.to_f >= 3.7
